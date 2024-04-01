0e278ead4ad10e9e97bfcb970568ad60b8a38f95
xshinigamix <carlosjraugusto3@gmail.com>
2024-04-01 17:15:47 -0300

    buttom end img
    ajeitei o visual dos buttoms

--- src/app/componentes/hero/Hero.tsx
+++ src/app/componentes/hero/Hero.tsx
@@ -18,14 +18,14 @@ const Hero = () => {
                 </div>
                      <img src="./assts/imagem/my end dev.jpg" alt="" />
             </div>
-            <div>
+            <div  className="tech-icon">
                 <div className="tech-icon">
                     <img src="./assts/imagem/HTML5_without_wordmark_color_Logo_PNG_Vector__SVG__Free_Download-removebg-preview (1).png" alt="" />
                 </div>
-                <div>
-                    <img src="./assts/imagem/JavaScript_Core_Language-removebg-preview.png" alt="" />
+                <div  className="tech-icon">
+                    <img src="./assts/imagem/Javascript_programming_language_icon-removebg-preview.png" alt="" />
                 </div>
-                <div>
+                <div  className="tech-icon">
                     <img src="./assts/imagem/CSS3_-_MI_PORTAL-removebg-preview.png" alt="" />
                 </div>
             </div>
